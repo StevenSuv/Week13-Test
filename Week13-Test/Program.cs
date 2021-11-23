98fd859dc8b61e0046e54798409a6e5d670c5952
Opilane TTHK <opilane@tthk.ee>
2021-11-23 16:11:22 +0200

    Test

---
+++ Week13-Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Week13_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string rootDirectory = $@"C:\Users\opilane\samples";
+
+            Console.WriteLine("Enter the folder's name:");
+            string userFolder = Console.ReadLine();
+
+            string newDirectoryFullPath = @$"{rootDirectory}\{userFolder}";
+            bool directoryExists = Directory.Exists(newDirectoryFullPath);
+
+            if (directoryExists)
+            {
+                Console.WriteLine($"{userFolder} already exists in Samples.");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+            }
+            else
+            {
+                Directory.CreateDirectory(newDirectoryFullPath);
+                Console.WriteLine($"{userFolder} does not exist. {userFolder} has been created");
+
+            }
+        }
+    }
+}
---
+++ Ylesanne 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Ylesanne_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(GetRandomFact());
+        }
+
+        public static string GetRandomFact()
+        {
+            string filePath = @"C:\Users\opilane\samples\test\funFacts.txt";
+            string[] funFactsOMG = File.ReadAllLines(filePath);
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(1, funFactsOMG.Length);
+            Console.WriteLine(funFactsOMG[0]);
+
+            return funFactsOMG[randomIndex];
+        }
+    }
+}
